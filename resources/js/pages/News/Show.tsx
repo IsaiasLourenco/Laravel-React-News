a95bf81ccc531034deb74a73c48d1c520bb7e1c7
Isaias <isaias.lourenco2020@outlook.com>
2025-11-23 15:29:42 -0300

    Details

--- resources/js/pages/News/Show.tsx
+++ resources/js/pages/News/Show.tsx
@@ -2,12 +2,12 @@ import { Head, usePage, router } from "@inertiajs/react";
 import { useEffect } from "react";
 
 interface News {
-  id: number;
-  titulo: string;
-  criado_por: string;
-  data_criacao: string;
-  conteudo: string;
-  imagem?: string | null;
+    id: number;
+    titulo: string;
+    criado_por: string;
+    data_criacao: string;
+    conteudo: string;
+    imagem?: string | null;
 }
 
 export default function Show({ news }: { news: News }) {
@@ -32,7 +32,7 @@ export default function Show({ news }: { news: News }) {
 
             {/* Fundo escuro semi-transparente */}
             <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-                
+
                 {/* Modal */}
                 <div className="bg-white dark:bg-neutral-900 rounded-xl shadow-lg max-w-3xl w-full max-h-screen overflow-y-auto p-6 relative">
 
@@ -46,7 +46,10 @@ export default function Show({ news }: { news: News }) {
 
                     <h1 className="text-2xl font-bold mb-4">{news.titulo}</h1>
                     <p className="text-sm text-gray-600 dark:text-gray-400 mb-4">
-                        Criado por {news.criado_por} em {news.data_criacao}
+                        Criado por {news.criado_por} em {""}
+                        {new Intl.DateTimeFormat("pt-BR").format(
+                            new Date(news.data_criacao + "T00:00:00")
+                        )}
                     </p>
 
                     {news.imagem && (
